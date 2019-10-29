3fd17f149350d306ac3955384410f185405d9ece
unknown <nahashon.satat@gmail.com>
2019-10-29 19:47:02 +0200

    adding shering objects ; num of shering; anther mat to graph and matrix transpose

--- function_try.py
+++ function_try.py
@@ -3,6 +3,7 @@ import networkx as nx
 import math as math
 import doctest as doctest
 import matplotlib.pyplot as plt
+import numpy as np
 
 def has_negative_cyc(digraph):
     """
@@ -52,6 +53,13 @@ def has_cyc_less_than_one(digraph):
        >>> q['c']['a']['weight'] = 1
     >>> has_negative_cyc(q)
     False
+    >>> q['c']['a']['weight'] = 10
+    >>> has_cyc_less_than_one(q)
+    True
+
+    >>> q['c']['a']['weight'] = 1000
+    >>> has_cyc_less_than_one(q)
+    False
     """
     g = nx.DiGraph()
     for (u, v, d) in digraph.edges(data=True):
@@ -64,31 +72,112 @@ def has_cyc_less_than_one(digraph):
     #return g
 
 
+def num_of_shared_objects(z):
+    """
+    return the number of object that shared = belong to more then on agent
+    :param z :  matrix  that represent - given allocation
+    :return:  number of shared objects
+
+    >>> z = [ [1, 0, 0],[0.8, 0.1, 0.1],[0, 1, 0]]
+    >>> num_of_shared_objects(z)
+    1
+    >>> z1 = [[1, 0, 0],[0.8, 0.1, 0.1],[0.2, 0.6, 0.2]]
+    >>> num_of_shared_objects(z1)
+    2
+    """
+    count = 0
+    for i in range(0, len(z)):
+        for j in range(0, len(z[0])):
+            if(z[i][j] == 1):
+                break
+            if (z[i][j] > 0):
+                count += 1
+                break
+    return count
 
 
+def num_of_sharing(mat):
+    """
+    return the number of sharing in given allocation
+    :param mat matrix that represent - given allocation
+    :return:  number of sharing
 
-def mat_to_directed_graph(matz , matv):
+    >>> z = [ [1, 0, 0],[0.8, 0.1, 0.1],[0, 1, 0]]
+    >>> num_of_sharing(z)
+    2
+    >>> z1 = [[1, 0, 0],[0.8, 0.1, 0.1],[0.2, 0.6, 0.2]]
+    >>> num_of_sharing(z1)
+    4
+    """
+    count = 0
+    temp = 0
+    for i in range(0, len(mat)):
+
+        for j in range(0, len(mat[0])):
+            if(mat[i][j] > 0)and(mat[i][j] < 1):
+                temp += 1
+        if not (temp == 0):
+            temp += -1
+            count += temp
+            temp = 0
+
+    return count
+def mat_to_directed_graph1(matz , matv):
     """
     convert the z matrix to directed graph
-    :param matrix: matz represent - given allocation , matv represent- the Agents value for the objects
+    :param matv:  represent- the Agents value for the objects
+    :param matz: represent - given allocation
     :return: directed_graph
 
     """
     n = len(matz)
-    m= len(matz[0])
+    m = len(matz[0])
     g = nx.DiGraph()
-    for i in range(0,len(matz)):
-        for j in range (0,len(matz[0])):
-            if(matz[i][j]<1):
-                if(matv[i][j]<0):
-                    g.add_edge("i"+str(i),"o"+str(j),weight=(matv[i][j]*-1))
+    for i in range(0, len(matz)):
+        for j in range(0, len(matz[0])):
+            if(matz[i][j] > 0):
+                if(matv[i][j] <= 0):
+                    g.add_edge("i"+str(i), "o"+str(j), weight=(matv[i][j]))
                 else:
-                    g.add_edge("o"+str(j),"i"+str(i),weight=1/(matv[i][j]))
+                    g.add_edge("o"+str(j), "i"+str(i), weight=1/(matv[i][j]))
 
     return g
 
 
+def mat_to_directed_graph2(matz, matv):
+    """
+    convert the z matrix to directed graph
+    :param matv:  represent- the Agents value for the objects
+    :param matz: represent - given allocation
+    :return: directed_graph
 
+    """
+    n = len(matz)
+    m = len(matz[0])
+    g = nx.DiGraph()
+    for i in range(0, len(matz)):
+        for j in range(0, len(matz[0])):
+            if(matz[i][j] < 1):
+                if(matv[i][j] > 0):
+                    g.add_edge("o"+str(i), "i"+str(j), weight=1/(matv[i][j]))
+                else:
+                    g.add_edge("i"+str(j), "o"+str(i), weight=(matv[i][j]*-1))
+
+    return g
+
+def matrix_transpose(mat):
+    """
+    this function transpose matrix
+    """
+    ans = [[row[i] for row in mat]for i in range(len(mat[0]))]
+    return ans
+
+def printmat(matrix):
+    """
+    this function print matrix in her shape
+    """
+    for row in matrix:
+        print(row)
 
 def print_graph(G):
     """
@@ -123,13 +212,12 @@ def print_graph(G):
 
 
 if __name__ == '__main__':
-    v = [[5, 1], [25, 2], [1, 5]]
-    z = [[1, 0], [0.8, 0.2], [0, 1]]
-    G = mat_to_directed_graph(z, v)
-    print_graph(G)
-  #(failures,tests) = doctest.testmod(report=True)
-  #print("{} failures, {} tests".format(failures,tests))
-
+    #v = [[5, 1], [25, 2], [1, 5]]
+    #z = [[1, 0], [0.8, 0.2], [0, 1]]
+    #G = mat_to_directed_graph(z, v)
+   # print_graph(G)
+  (failures, tests) = doctest.testmod(report=True)
+  print("{} failures, {} tests".format(failures, tests))
 
 """
 def test1():
