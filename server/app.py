a8105e9bc1a61ea4da2a8e3d73a176ad2eed4ec1
DanielAbergel <danielabergel1@gmail.com>
2020-10-12 21:52:35 +0300

    adding full email support with html template

--- server/app.py
+++ server/app.py
@@ -1,11 +1,14 @@
 import numpy as np
 import sys
+from _sha256 import sha256
 from threading import Thread
 import smtplib
 import ssl
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-import os
+from dominate import document
+from dominate.tags import *
+from string import Template
 
 sys.path.append('../')
 from flask import Flask, request, render_template, jsonify, send_from_directory
@@ -15,6 +18,7 @@ from algorithm.Version3.FairProportionalAllocationProblem import FairProportiona
 
 app = Flask(__name__)
 api = Api(app)
+URL = '161.35.20.108'
 
 
 @app.route('/')
@@ -42,23 +46,68 @@ def send_images(image):
     return send_from_directory('../web/images', image)
 
 
-@app.route('/result/<string:data>')
+@app.route('/generated_html/<string:data>')
 def build_result(data):
-    print(data)
-    return send_from_directory('../web/', 'Home.html')
+    return send_from_directory('../web/generated_html/', data)
 
 
-def genrate_result_table(data):
+def generate_table(agents, items, data, file_name):
+    with document() as doc:
+        head_page = head()
+        head_page += link(rel='shortcut icon', href="../images/web-logo.png")
+        head_page += link(rel="stylesheet", type="text/css", href="../css/Home.css")
+        head_page += link(href="https://fonts.googleapis.com/css2?family=Special+Elite&display=swap", rel="stylesheet")
+        head_page += link(href="https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap",
+                          rel="stylesheet")
+        link(rel="stylesheet", href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css",
+             integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm",
+             crossorigin="anonymous")
+        link(rel="stylesheet", type="text/css", href="../css/styles.css")
+        content_div = div(cls='topnav')
+        content_div += a('Home', cls='active', href='../Home.html')
+        content_div += a('About Us', cls='active', href='../About_Us.html')
+        content_div += a('Contact us', cls='active', href='../Contact_Us.html')
+
+        h1('Results', cls='header')
+
+        result_table = table(cls='content-table')
+        link(rel='stylesheet', href='../css/table.css')
+        header_thread = thead()
+        header_tr = tr()
+        header_tr += th('Items')
+        for agent in agents:
+            header_tr += th(agent)
+        header_thread += header_tr
+
+        item_index = 0
+
+        thread_body = tbody()
+        for item in items:
+            item_tr = tr()
+            item_tr += th(item)
+            for score in data[item_index]:
+                item_tr += td('{} %'.format(score))
+            item_index += 1
+            thread_body += item_tr
+
+        result_table += header_thread
+        result_table += thread_body
+
+    with open('../web/generated_html/{}.html'.format(file_name), 'w') as f:
+        f.write(doc.render())
+    return '{}/generated_html/{}.html'.format(URL, file_name)
 
 
 def long_time_algorithm(data):
-    send_email(data['email'], data['values'])
+    url = generate_table(agents=data['agents'], items=data['items'],
+                         data=data['values'], file_name=sha256(str(data['values']).encode('utf-8')).hexdigest())
+    send_email(data['email'], url)
 
 
-def send_email(email_receiver, values):
+def send_email(email_receiver, url):
     sender_email = "fairnessalgorithm.io@gmail.com"
     receiver_email = email_receiver
-    password = os.environ['EMAIL_PASSWORD']
+    password = ''
 
     message = MIMEMultipart("alternative")
     message["Subject"] = "Fairness.io Results"
@@ -66,16 +115,16 @@ def send_email(email_receiver, values):
     message["To"] = receiver_email
 
     # Create the plain-text and HTML version of your message
-    text = """need to complete here"""
-    html = """need to complete here"""
+
+    with open('../web/html_email_template.html', 'r+') as f:
+        template = Template(f.read())
+        html = (template.substitute(URL=url))
 
     # Turn these into plain/html MIMEText objects
-    part1 = MIMEText(text, "plain")
     part2 = MIMEText(html, "html")
 
     # Add HTML/plain-text parts to MIMEMultipart message
     # The email client will try to render the last part first
-    message.attach(part1)
     message.attach(part2)
 
     # Create secure connection with server and send email
@@ -98,9 +147,8 @@ class Algorithm(Resource):
 
         if data['num_of_agents'] > 3:
 
-            # for email support will finish it later.
-            # thread = Thread(target=long_time_algorithm, daemon=True)
-            # thread.start()
+            thread = Thread(target=long_time_algorithm, args=(data,), daemon=True)
+            thread.start()
 
             json_request = {
                 'message': 'to many agents , the algorithm results will send by email.',
@@ -116,13 +164,16 @@ class Algorithm(Resource):
             if data['problem'] == 'EnvyFree':
                 ProblemObject = FairEnvyFreeAllocationProblem(matrix)
                 ans = ProblemObject.find_allocation_with_min_shering()
+                print('Using EnvyFree Algorithm')
             elif data['problem'] == 'Proportional':
                 ProblemObject = FairProportionalAllocationProblem(matrix)
                 ans = ProblemObject.find_allocation_with_min_shering()
+                print('Using Proportional Algorithm')
 
             json_request = {
-                'num_of_agents': data['num_of_agents'],
-                'num_of_items': data['num_of_items'],
+                'problem': data['problem'],
+                'agents': data['agents'],
+                'items': data['items'],
                 'values': ans.tolist()
             }
 
