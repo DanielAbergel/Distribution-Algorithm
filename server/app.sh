d72f18193a5e0309ef6f5ab889edf56297e2570b
DanielAbergel <danielabergel1@gmail.com>
2020-10-19 13:46:02 +0300

    upload app.sh script for debug development

---
+++ server/app.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+isBuild=$1
+password=$2
+build='build'
+
+if [ "$isBuild" == "$build" ]; then
+    echo "Building server for DEBUG purposes"
+
+    echo $password | sudo -S pip3 install virtualenv
+    echo Y | sudo -S apt-get install virtualenv
+    virtualenv venv --python=python3
+    source venv/bin/activate
+    echo "-- Install server dependencies --"
+    pip install -r requirements.txt
+    deactivate
+else
+    echo "Runing DEBUG Server on 'http://127.0.0.1:5000' "
+    echo "-- Activating virtual environment --"
+    source venv/bin/activate
+    echo "-- Running app.py  --"
+    python app.py
+fi
